8a90be71cbf27a9f6687712937774e35a915ec67
Angus Kellett <anguskellett@hotmail.com>
2021-02-25 21:33:59 +0000

    creates and removes findUrl function (was not necessary)

--- helpers/userFunctions.js
+++ helpers/userFunctions.js
@@ -1,7 +1,6 @@
 const validateUser = function(email, password, database) {
   let response = { user: null, error: "email" }
   for (let user in database) {
-    console.log(user);
     if (database[user].email === email) {
       if (database[user].password === password) {
         response.user = database[user];
@@ -15,8 +14,6 @@ const validateUser = function(email, password, database) {
 };
 
 const urlsForUser = function(id, database) {
-  console.log(id);
-  console.log(database);
   const myUrls = {};
   for (let url in database) {
    if(database[url].userID === id) {
