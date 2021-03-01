c7cc0fa66f60a45059247b8e14a29fac02e3f61a
Angus Kellett <anguskellett@hotmail.com>
2021-03-01 06:01:49 +0000

    adds test for email that doesn't exist

---
+++ test/helpersTest.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers/userFunctions.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email and password', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedOutput = "userRandomID";
+    assert.strictEqual(user.id, expectedOutput);
+  });
+  it('should return <undefined> if an email is not in the database', function() {
+    const expectedOutput = undefined;
+    assert.strictEqual(getUserByEmail("myfavouriteband@wutangclan.com", testUsers), expectedOutput);
+  });
+});
