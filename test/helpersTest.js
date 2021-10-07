aef5c60897bd62786483d50ec9a6a410b638998d
Angus Kellett <anguskellett@hotmail.com>
2021-10-07 04:16:36 +0000

    changes double quotes to single; removes semicolons

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,28 +1,27 @@
-const { assert } = require('chai');
+const { assert } = require('chai')
 
-const { validateUser } = require('../helpers/userFunctions.js');
+const { validateUser } = require('../helpers/userFunctions.js')
 
 const testUsers = {
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
   },
-  "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
+  user2RandomID: {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk'
   }
-};
+}
 
-describe('validateUser', function() {
-  it('should return email error if email not present in database', function() {
-    const expectedOutput = { user: null, error: "email" };
-    assert.deepEqual(validateUser("dracula@transylvania.com", "purple-monkey-dinosaur", testUsers), expectedOutput);
-  });
-  it('should return password error if email is present in database but password is incorrect', function() {
-    const expectedOutput = { user: null, error: "password" };
-    assert.deepEqual(validateUser("user@example.com", "iwanttosuckyourblood", testUsers), expectedOutput);
-  });
-});
-
+describe('validateUser', function () {
+  it('should return email error if email not present in database', function () {
+    const expectedOutput = { user: null, error: 'email' }
+    assert.deepEqual(validateUser('dracula@transylvania.com', 'purple-monkey-dinosaur', testUsers), expectedOutput)
+  })
+  it('should return password error if email is present in database but password is incorrect', function () {
+    const expectedOutput = { user: null, error: 'password' }
+    assert.deepEqual(validateUser('user@example.com', 'iwanttosuckyourblood', testUsers), expectedOutput)
+  })
+})
